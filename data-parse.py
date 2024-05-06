c917feeb3808d9e01ae3d4db5d972a2d4c698a79
Smith Gakuya <sgakuya@middlebury.edu>
2024-05-06 17:30:51 -0400

    First commit

---
+++ data-parse.py
@@ -0,0 +1,116 @@
+import csv
+from student import Student
+# /Users/sgakuya@middlebury.edu/Downloads/f24.csv
+
+class DataParser:
+    def __init__(self, filename):
+        # create an empty list of students
+        self.students = []
+
+        # open the file from Google forms
+        with open(filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+
+            # go through each row in file
+            for row in csv_reader:
+                # skip first row
+                if line_count > 0:
+                    # get email, name, id number, and employment status from sheet
+                    email = row[1]
+                    name = row[2]
+                    nickname = row[3]
+                    id_num = row[4]
+                    
+                    # set courses and upper level courses
+                    courses = row[5].split(', ')
+                    upperlevel_courses = row[6]
+
+                    # add grading/ca experience and preferences for all courses 
+                    fa_24_classes = ["CS 105", "CS 145 / CS 146", "CS 200", "CS 201", "CS 202", "CS 301", "CS 302", "CS 311", "CS 312", "CS 315", "CS 333"]
+                    # h-r(7-17) => grader , s-ac(18-28) => ca
+                    grade_courses = []
+                    grade_experience = []
+                    ca_courses = []
+                    ca_experience = []
+                    for i in range(7,29):
+                        if(i<18):
+                            if "experience" in row[i]:
+                                grade_experience.append(fa_24_classes[i-7])
+                            elif "willing" in row[i] or "preferred" in row[i]:
+                                grade_courses.append(fa_24_classes[i-7])
+                        else:
+                            if "experience" in row[i]:
+                                ca_experience.append(fa_24_classes[i-18])
+                            elif "willing" in row[i] or "preferred" in row[i]:
+                                ca_courses.append(fa_24_classes[i-18])
+
+                    # set courses they can be inclass CAs for
+                    temp_inclass = row[29].split(',')
+                    inclass = []
+                    for i in range(0,len(temp_inclass), 3):
+                        if len(temp_inclass) > 1:
+                            inclass.append(temp_inclass[i]+temp_inclass[i+1]+temp_inclass[i+2])
+                    
+                    # set preference for grader vs CA
+                    preference = row[30]
+
+                    # set # of CA hours
+                    hours = 0
+                    match row[31]:
+                        case "I don't want to work as a course assistant":
+                            hours = 0
+                        case "2-3":
+                            hours = 2
+                        case "4-5":
+                            hours = 4
+                        case "6+":
+                            hours =6
+
+                    # set CTLR preference
+                    ctlr = row[32]
+                    ctlr_courses = row[33]
+
+                    # set interest in being the social CA
+                    social_ca = row[34]
+
+                    # comments
+                    comments = row[35]
+
+                    # create student object
+                    student = Student(email, name, nickname, id_num, courses,
+                             upperlevel_courses, grade_courses, grade_experience, ca_courses, ca_experience,
+                             inclass, preference, hours, comments)
+
+                    # add student object to list
+                    self.students.append(student)
+
+                line_count += 1
+
+        count = 1
+        for std in self.students:
+            print(count, std)
+            count += 1
+
+    # this method sorts the students by last name, then by first name
+    def sort_students(self):
+
+        # sort students by last name
+        self.students.sort(key = lambda x: x.name.split()[-1], reverse = False)
+
+        # now sort by first name if last names are the same
+        sort = False
+        while sort == False:
+            sort = True
+            for i in range(1, len(self.students)):
+                student = self.students[i]
+                prev_student = self.students[i-1]
+
+                if student.name.split()[-1] == prev_student.name.split()[-1]: # last names
+                    if student.name.split()[0] < prev_student.name.split()[0]: # first names
+                        self.students[i-1] = student
+                        self.students[i] = prev_student
+                        sort = False
+
+if __name__ == "__main__":
+    DataParser("/Users/sgakuya@middlebury.edu/Downloads/f24.csv")
---
+++ student.py
@@ -0,0 +1,85 @@
+class Student:
+
+# email, name, nickname, id_num, courses,
+#                              upperlevel_courses, grade_courses, grade_experience, ca_courses, ca_experience,
+#                              inclass, preference, hours, comments)
+    def __init__(self, email, name, nickname, id_num, courses,
+                             upperlevel_courses, grade_courses, grade_experience, ca_courses, ca_experience,
+                             ca_inclass, preference, hours, comments):
+
+        # store name and email
+        self.name = name
+        self.email = email
+
+        # store nickname and ID number
+        self.nickname = nickname
+        self.id = id_num
+
+        # allow students who tested out of CS 101/145/150 to tutor for CS 101/145/150
+        if 'CS 101/145' not in courses and 'CS 150' not in courses and ('CS 200' in courses or 'CS 201' in courses):
+            courses.append('CS 145')
+            courses.append('CS 150')
+
+        # we'll just disregard 101 and convert all instances of 101/145 to 145
+        if 'CS 101/145' in courses:
+            courses.append('CS 145')
+            courses.remove('CS 101/145')
+
+        # add CS 105 to courses in student has taken 101, 145, or 150
+        if 'CS 145' in courses or 'CS 150' in courses:
+            courses.append('CS 105')
+
+        # for now we are keeping CS 145 and CS 150 tutors separate... but can change this with
+        # the following code
+
+        # if the student has taken 101/145, allow them to tutor for 150
+        #if ('CS 101' in courses or 'CS 145' in courses) and 'CS 150' not in courses:
+        #    courses.append('CS 150')
+        #
+        # if the student has taken 150, all them to tutor for 101/145
+        #elif 'CS 150' in courses and 'CS 101' not in courses and 'CS 145' not in courses:
+        #    courses.append('CS 101')
+        #    courses.append('CS 145')
+
+        # set list of courses a student has taken
+        self.courses = courses
+
+        # set list of upper level courses a student has taken
+        self.upperlevel_courses = upperlevel_courses
+
+        # set list of courses student wants to grade for
+        self.grade_courses = grade_courses
+
+        # set list of courses student has experience grading for
+        if grade_experience != False:
+            if 'CS 101' in grade_experience:
+                grade_experience.append('CS 145')
+        self.grade_experience = grade_experience
+
+        # set list of courses student wants to tutor for
+        self.ca_courses = ca_courses
+
+         # set list of courses student has experience tutoring for
+        if ca_experience != False:
+            if 'CS 101/145' in ca_experience:
+                ca_experience.append('CS 145')
+                ca_experience.remove('CS 101/145')
+        self.ca_experience = ca_experience
+
+        # set list of lab classes student wants to tutor for
+        self.ca_inclass = ca_inclass
+
+        # set preference for tutoring or grading
+        self.preference = preference
+
+        # num of shifts willing to work
+        self.hours = hours
+
+        # set comments
+        self.comments = comments
+
+
+    # this method returns the student's name if print()
+    # is called on a Student object
+    def __str__(self):
+        return (self.name + " => " + self.preference)
